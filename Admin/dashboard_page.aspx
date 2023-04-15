a36000619477c95e3b910f13988b9a3e4b07bbd8
Suman Dangol <sdangol368@gmail.com>
2023-04-15 08:43:35 +0545

    Dmin features are added

--- Admin/dashboard_page.aspx
+++ Admin/dashboard_page.aspx
@@ -2,5 +2,82 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>Dashboard</h1>
+    
+    <div id="page-content-wrapper ">
+            <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4 border-dark border-bottom">
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
+                </div>
+
+
+                <div  id="navbarSupportedContent">
+                <h3> <strong>User</strong> </h3>     
+                </div>
+            </nav>
+         <div class="container-fluid px-4 my-5">
+                <div class="row g-3 my-2">
+                    <div class="col-md-6 col-lg-6 ">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">720</h3>
+                                <p class="fs-5">Products</p>
+                            </div>
+                            <i class="fas fa-gift fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-6">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">4920</h3>
+                                <p class="fs-5">Sales</p>
+                            </div>
+                            <i
+                                class="fas fa-hand-holding-usd fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-6">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">3899</h3>
+                                <p class="fs-5">Delivery</p>
+                            </div>
+                            <i class="fas fa-truck fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-6">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">%25</h3>
+                                <p class="fs-5">Increase</p>
+                            </div>
+                            <i class="fas fa-chart-line fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">%25</h3>
+                                <p class="fs-5">Increase</p>
+                            </div>
+                            <i class="fas fa-chart-line fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-6">
+                        <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                            <div>
+                                <h3 class="fs-2">%25</h3>
+                                <p class="fs-5">Increase</p>
+                            </div>
+                            <i class="fas fa-chart-line fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                        </div>
+                    </div>
+                </div> 
+        </div>
+    </div>
+
+
+
 </asp:Content>
--- Admin/flight_page.aspx.cs
+++ Admin/flight_page.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using OnlineAirwayTicketBooking.Utils;
 
 namespace OnlineAirwayTicketBooking.Admin
 {
@@ -13,5 +16,112 @@ namespace OnlineAirwayTicketBooking.Admin
         {
 
         }
+
+        public void loadFlightData()
+        {
+            Myconnection dbConnect = new Myconnection();
+            string cmdText = "SELECT f.flight_id, f.flight_number, f.departure_airport, f.arrival_airport , f.departure_time , f.arrival_time , f.duration, a.airline_name FROM flights f " +
+                "INNER JOIN airlines a on a.airline_id = f.airline_id";
+            SqlCommand cmd = new SqlCommand(cmdText, dbConnect.connect);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            GridViewFlight.DataSource = dt;
+            GridViewFlight.DataBind();
+            dbConnect.connect.Close();
+
+        }
+
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            Myconnection dbConnect = new Myconnection();
+            string cmdText = "INSERT INTO flights (flight_number, departure_airport, arrival_airport , departure_time , arrival_time , duration, airline_id) VALUES" +
+                " (@flight_number, @departure_airport, @arrival_airport , @departure_time , @arrival_time , @duration, @airline_id)";
+            SqlCommand cmd = new SqlCommand(cmdText, dbConnect.connect);
+            cmd.Parameters.AddWithValue("@flight_number", txtFlightNumber.Text);
+            cmd.Parameters.AddWithValue("@departure_airport", txtDepartureAirport.Text);
+            cmd.Parameters.AddWithValue("@arrival_airport", txtArrivalAirport.Text);
+            cmd.Parameters.AddWithValue("@departure_time", txtDepartureTime.Text);
+            cmd.Parameters.AddWithValue("@arrival_time", txtArrivalTime.Text);
+            cmd.Parameters.AddWithValue("@duration", Convert.ToInt32(txtDuration.Text));
+            cmd.Parameters.AddWithValue("@airline_id", Convert.ToInt32(txtAirlineId.Text));
+            cmd.ExecuteNonQuery();
+            dbConnect.connect.Close();
+
+            loadFlightData();
+
+            clearFiledds();
+
+        }
+
+        protected void GridViewFlight_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewFlight.EditIndex = e.NewEditIndex;
+            loadFlightData();
+        }
+        protected void GridViewFlight_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+            int id = Convert.ToInt32(GridViewFlight.DataKeys[e.RowIndex].Value);
+            string flight_number = e.NewValues["flight_number"].ToString();
+            string departure_airport = e.NewValues["departure_airport"].ToString();
+            string arrival_airport = e.NewValues["arrival_airport"].ToString();
+            string departure_time = e.NewValues["departure_time"].ToString();
+            string arrival_time = e.NewValues["arrival_time"].ToString();
+            string duration = e.NewValues["duration"].ToString();
+
+            Myconnection dbConnect = new Myconnection();
+            string cmdText = "UPDATE flights SET flight_number=@flight_number, departure_airport=@departure_airport, arrival_airport=@arrival_airport , departure_time=@departure_time ," +
+                " arrival_time=@arrival_time , duration=@duration WHERE flight_id=@id";
+            SqlCommand cmd = new SqlCommand(cmdText, dbConnect.connect);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@flight_number", flight_number);
+            cmd.Parameters.AddWithValue("@departure_airport", departure_airport);
+            cmd.Parameters.AddWithValue("@arrival_airport", arrival_airport);
+            cmd.Parameters.AddWithValue("@departure_time", departure_time);
+            cmd.Parameters.AddWithValue("@arrival_time", arrival_time);
+            cmd.Parameters.AddWithValue("@duration", duration);
+            cmd.ExecuteNonQuery();
+            dbConnect.connect.Close();
+            GridViewFlight.EditIndex = -1;
+
+            loadFlightData();
+        }
+        protected void GridViewFlight_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
+            GridViewFlight.EditIndex = -1;
+            loadFlightData();
+        }
+
+
+        protected void OnPageIndexChanging_Flight(object sender, GridViewPageEventArgs e)
+        {
+
+            GridViewFlight.PageIndex = e.NewPageIndex;
+            loadFlightData();
+
+        }
+
+        private void clearFiledds()
+        {
+            txtAirlineId.Text = string.Empty;
+            txtArrivalAirport.Text = string.Empty;
+            txtDepartureAirport.Text = string.Empty;
+            txtArrivalTime.Text = string.Empty;
+            txtDepartureTime.Text = string.Empty;
+            txtDuration.Text = string.Empty;
+            txtFlightNumber.Text = string.Empty;
+
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            clearFiledds();
+
+        }
     }
+
 }
